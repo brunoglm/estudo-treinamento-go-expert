223aa67bd3b86d46aa43537909708a0ff76a05f2
Bruno Gabriel <bruno.glm_@hotmail.com>
2024-07-11 10:48:54 -0300

    add content

--- 2-pacotes-importantes/4-busca-cep-cli/main.go
+++ 2-pacotes-importantes/4-busca-cep-cli/main.go
@@ -26,24 +26,12 @@ type ViaCep struct {
 // go run main.go 01001000
 func main() {
 	for _, cep := range os.Args[1:] {
-		resp, err := http.Get(fmt.Sprintf("https://viacep.com.br/ws/%s/json/", cep))
-		if err != nil {
-			fmt.Fprintf(os.Stderr, err.Error())
-		}
-		defer resp.Body.Close()
-
-		res, err := io.ReadAll(resp.Body)
+		viaCep, err := buscaCep(cep)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, err.Error())
 		}
 
-		var cep ViaCep
-		err = json.Unmarshal(res, &cep)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, err.Error())
-		}
-
-		err = json.NewEncoder(os.Stdout).Encode(cep)
+		err = json.NewEncoder(os.Stdout).Encode(viaCep)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, err.Error())
 		}
@@ -54,6 +42,27 @@ func main() {
 		}
 		defer file.Close()
 
-		_, err = file.WriteString(fmt.Sprintf("CEP: %s, Localidade: %s, UF: %s", cep.Cep, cep.Localidade, cep.Uf))
+		_, err = file.WriteString(fmt.Sprintf("CEP: %s, Localidade: %s, UF: %s", viaCep.Cep, viaCep.Localidade, viaCep.Uf))
 	}
+}
+
+func buscaCep(cep string) (*ViaCep, error) {
+	resp, err := http.Get(fmt.Sprintf("https://viacep.com.br/ws/%s/json/", cep))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	res, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var viaCep ViaCep
+	err = json.Unmarshal(res, &viaCep)
+	if err != nil {
+		return nil, err
+	}
+
+	return &viaCep, nil
 }
