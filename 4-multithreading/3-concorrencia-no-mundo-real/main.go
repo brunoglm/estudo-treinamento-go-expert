8daab474a125d4018ef0034e5f0c569478aabf6c
Bruno Gabriel <bruno.glm_@hotmail.com>
2024-07-15 00:51:05 -0300

    add content

--- 4-multithreading/3-concorrencia-no-mundo-real/main.go
+++ 4-multithreading/3-concorrencia-no-mundo-real/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 )
 
 var number uint64 = 0
@@ -11,7 +12,7 @@ func main() {
 	// mu := sync.Mutex{}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		// mu.Lock()
-		number++
+		atomic.AddUint64(&number, 1)
 		w.Write([]byte(fmt.Sprintf("Você teve acesso a essa página %d vezes\n", number)))
 		// mu.Unlock()
 	})
