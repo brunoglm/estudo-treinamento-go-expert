67133d70e3be967cdd962d604f9c5fb137798aed
Bruno Gabriel <bruno.glm_@hotmail.com>
2024-08-11 23:29:31 -0300

    add content

---
+++ 7-testing/1-iniciando-com-testes/go.mod
@@ -0,0 +1,3 @@
+module tax-go
+
+go 1.22.4
---
+++ 7-testing/1-iniciando-com-testes/tax_test.go
@@ -0,0 +1,63 @@
+package tax
+
+import "testing"
+
+func TestCalculateTax(t *testing.T) {
+	amount := 500.0
+	expected := 5.0
+
+	result := CalculateTax(amount)
+
+	if result != expected {
+		t.Errorf("Expected %f but got %f", expected, result)
+	}
+}
+
+func TestCalculateTaxBatch(t *testing.T) {
+	type calcTax struct {
+		amount, expected float64
+	}
+
+	table := []calcTax{
+		{500.0, 5.0},
+		{1000.0, 10.0},
+		{1500.0, 10.0},
+		{0.0, 0.0},
+	}
+
+	for _, v := range table {
+		result := CalculateTax(v.amount)
+
+		if result != v.expected {
+			t.Errorf("Expected %f but got %f", v.expected, result)
+		}
+	}
+}
+
+func BenchmarkCalculateTax(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		CalculateTax(500.0)
+	}
+}
+
+func BenchmarkCalculateTax2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		CalculateTax2(500.0)
+	}
+}
+
+func FuzzCalculateTax2(f *testing.F) {
+	seed := []float64{-1, -2, -2.5, 500.0, 1000.0, 1501.0}
+	for _, amount := range seed {
+		f.Add(amount)
+	}
+	f.Fuzz(func(t *testing.T, amount float64) {
+		result := CalculateTax(amount)
+		if amount <= 0 && result != 0 {
+			t.Errorf("Received %f but expected 0", result)
+		}
+		if amount > 20000 && result != 20 {
+			t.Errorf("Received %f but expected 20", result)
+		}
+	})
+}
