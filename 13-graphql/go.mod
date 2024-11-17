b72c97929e8c8f8ef7621b005e5576772ea4ed80
Bruno Gabriel <bruno.glm_@hotmail.com>
2024-11-17 14:47:19 -0300

    add content

---
+++ 13-graphql/go.mod
@@ -0,0 +1,29 @@
+module graphql
+
+go 1.22.5
+
+toolchain go1.22.9
+
+require (
+	github.com/99designs/gqlgen v0.17.56
+	github.com/google/uuid v1.6.0
+	github.com/mattn/go-sqlite3 v1.14.24
+	github.com/vektah/gqlparser/v2 v2.5.19
+)
+
+require (
+	github.com/agnivade/levenshtein v1.1.1 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.5 // indirect
+	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/sosodev/duration v1.3.1 // indirect
+	github.com/urfave/cli/v2 v2.27.5 // indirect
+	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
+	golang.org/x/mod v0.20.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
+	golang.org/x/tools v0.24.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ 13-graphql/internal/database/category.go
@@ -0,0 +1,56 @@
+package database
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/google/uuid"
+)
+
+type Category struct {
+	db          *sql.DB
+	ID          string
+	Name        string
+	Description string
+}
+
+func NewCategory(db *sql.DB) *Category {
+	return &Category{db: db}
+}
+
+func (c *Category) Create(ctx context.Context, name string, description string) (Category, error) {
+	id := uuid.New().String()
+	_, err := c.db.ExecContext(ctx, "INSERT INTO categories (id, name, description) VALUES ($1, $2, $3)",
+		id, name, description)
+	if err != nil {
+		return Category{}, err
+	}
+	return Category{ID: id, Name: name, Description: description}, nil
+}
+
+func (c *Category) FindAll(ctx context.Context) ([]Category, error) {
+	rows, err := c.db.QueryContext(ctx, "SELECT id, name, description FROM categories")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	categories := []Category{}
+	for rows.Next() {
+		var category Category
+		if err := rows.Scan(&category.ID, &category.Name, &category.Description); err != nil {
+			return nil, err
+		}
+		categories = append(categories, category)
+	}
+	return categories, nil
+}
+
+func (c *Category) FindByCourseID(ctx context.Context, courseID string) (Category, error) {
+	var category Category
+	err := c.db.QueryRowContext(ctx, "SELECT c.id, c.name, c.description FROM categories c JOIN courses co ON c.id = co.category_id WHERE co.id = $1", courseID).
+		Scan(&category.ID, &category.Name, &category.Description)
+	if err != nil {
+		return Category{}, err
+	}
+	return category, nil
+}
