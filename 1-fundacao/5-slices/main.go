2a00cddc0bc32ddfc353cb4ed0040faa480e7c22
Bruno Gabriel <bruno.glm_@hotmail.com>
2025-05-18 11:52:08 -0300

    add content

--- 1-fundacao/5-slices/main.go
+++ 1-fundacao/5-slices/main.go
@@ -4,22 +4,33 @@ import "fmt"
 
 func main() {
 	s := []int{2, 4, 6, 8, 10, 12}
+	// tamanhos iniciais len=6 cap=6
 	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
 
-	fmt.Printf("len=%d cap=%d %v\n", len(s[:0]), cap(s[:0]), s[:0]) // len=0 cap=6
+	// len=0 cap=6
+	// capacidade é 6, pois o slice ainda aponta para o array original
+	fmt.Printf("len=%d cap=%d %v\n", len(s[:0]), cap(s[:0]), s[:0])
 
-	fmt.Printf("len=%d cap=%d %v\n", len(s[:4]), cap(s[:4]), s[:4]) // len=4 cap=6
+	// len=4 cap=6
+	// capacidade é 6, pois o slice ainda aponta para o array original
+	fmt.Printf("len=%d cap=%d %v\n", len(s[:4]), cap(s[:4]), s[:4])
 
-	fmt.Printf("len=%d cap=%d %v\n", len(s[2:]), cap(s[2:]), s[2:]) // len=4 cap=4
+	// len=4 cap=4
+	// capacidade é 4, mas o slice aponta para o array original ainda
+	fmt.Printf("len=%d cap=%d %v\n", len(s[2:]), cap(s[2:]), s[2:])
 
 	s = append(s, 14)
-	fmt.Printf("len=%d cap=%d %v\n", len(s[2:]), cap(s[2:]), s[2:]) // len=5 cap=12
+	// len=5 cap=10
+	// capacidade é 10, pois foi criado um novo array para o slice, com o dobro do tamanho
+	fmt.Printf("len=%d cap=%d %v\n", len(s[2:]), cap(s[2:]), s[2:])
+
+	fmt.Println("O slice original é", s)
 
 	testeAlterandoValorPorPonteiro(s)
 
-	for i, v := range s {
-		fmt.Printf("O valor do indice é %d e o valor é %d\n", i, v)
-	}
+	fmt.Println("O slice após testeAlterandoValorPorPonteiro é", s)
+
+	// poc com copy e criação normal
 }
 
 func testeAlterandoValorPorPonteiro(array []int) {
