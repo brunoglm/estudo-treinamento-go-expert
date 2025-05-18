67c0ed178bbf96be5eef04716f9faf71d5926735
Bruno Gabriel <bruno.glm_@hotmail.com>
2025-05-18 11:25:24 -0300

    add content

--- 1-fundacao/4-percorrendo-arrays/main.go
+++ 1-fundacao/4-percorrendo-arrays/main.go
@@ -7,12 +7,28 @@ func main() {
 	array[0] = 10
 	array[1] = 20
 	array[2] = 30
-	testeAlterandoValorPorPonteiro(&array)
-	fmt.Println(len(array))          // tamanho 3
-	fmt.Println(cap(array))          // capacidade 3
-	fmt.Println(len(array) - 1)      // index da ultima posição sendo 2
-	fmt.Println(array[len(array)-1]) // valor da ultima posição sendo 50
 
+	fmt.Println("Dados do array inicial:")
+	fmt.Println(len(array)) // tamanho 3
+	fmt.Println(cap(array)) // capacidade 3
+	for i, v := range array {
+		fmt.Printf("O valor do indice é %d e o valor é %d\n", i, v)
+	}
+
+	testeAlterandoValorSemSerPorPonteiro(array)
+
+	fmt.Println("Dados do array após testeAlterandoValorSemSerPorPonteiro:")
+	fmt.Println(len(array)) // tamanho 3
+	fmt.Println(cap(array)) // capacidade 3
+	for i, v := range array {
+		fmt.Printf("O valor do indice é %d e o valor é %d\n", i, v)
+	}
+
+	testeAlterandoValorPorPonteiro(&array)
+
+	fmt.Println("Dados do array após testeAlterandoValorPorPonteiro:")
+	fmt.Println(len(array)) // tamanho 3
+	fmt.Println(cap(array)) // capacidade 3
 	for i, v := range array {
 		fmt.Printf("O valor do indice é %d e o valor é %d\n", i, v)
 	}
@@ -20,4 +36,8 @@ func main() {
 
 func testeAlterandoValorPorPonteiro(array *[3]int) {
 	array[2] = 50
+}
+
+func testeAlterandoValorSemSerPorPonteiro(array [3]int) {
+	array[1] = 50
 }
