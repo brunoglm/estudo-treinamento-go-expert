75ec47a972d36fc42b77386ced2e18821ef4bfcd
Bruno Gabriel <bruno.glm_@hotmail.com>
2025-05-20 17:43:35 -0300

    add content

--- 3-context/3-withvalue/main.go
+++ 3-context/3-withvalue/main.go
@@ -6,6 +6,12 @@ import (
 )
 
 func main() {
+	// context é imutável
 	ctx := context.WithValue(context.Background(), "token", "senha123")
+	ctx = context.WithValue(ctx, "userID", 42)
+	ctx = context.WithValue(ctx, "role", "admin")
+
 	fmt.Println(ctx.Value("token"))
+	fmt.Println(ctx.Value("userID"))
+	fmt.Println(ctx.Value("role"))
 }
