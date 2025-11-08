785a7bcd2299bb999007bc471fdbc9fd9d5d5919
Bruno Gabriel <bruno.glm_@hotmail.com>
2025-11-08 12:44:28 -0300

    add content

---
+++ 15-upload-s3/cmd/generator/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	i := 0
+	for {
+		f, err := os.Create(fmt.Sprintf("./tmp/file%d.txt", i))
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+		f.WriteString("Hello, World!")
+		i++
+		if i >= 4 {
+			break
+		}
+	}
+}
---
+++ 15-upload-s3/go.mod
@@ -0,0 +1,3 @@
+module upload-s3
+
+go 1.25.2
