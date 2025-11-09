211f692bbf8376c7a3309f8080d4c8647f88a23d
Bruno Gabriel <bruno.glm_@hotmail.com>
2025-11-08 23:29:17 -0300

    add content

--- 15-upload-s3/cmd/uploader/main.go
+++ 15-upload-s3/cmd/uploader/main.go
@@ -53,6 +53,10 @@ func main() {
 		}
 		uploadFile(files[0].Name())
 	}
+
+	// listFiles := listFiles()
+	// deleteFile(*listFiles[0].Key)
+	// moveFile()
 }
 
 func uploadFile(filename string) {
@@ -78,4 +82,56 @@ func uploadFile(filename string) {
 	}
 
 	fmt.Printf("File %s uploaded successfully\n", completeFileName)
+}
+
+func listFiles() []*s3.Object {
+	resp, err := s3Client.ListObjectsV2(&s3.ListObjectsV2Input{
+		Bucket:    aws.String(s3Bucket),
+		Prefix:    aws.String("pending/"),
+		Delimiter: aws.String("/"),
+	})
+	if err != nil {
+		fmt.Printf("Error listing objects: %v\n", err)
+		return nil
+	}
+
+	fmt.Println(resp.Contents)
+	return resp.Contents
+}
+
+func deleteFile(fileName string) {
+	_, err := s3Client.DeleteObject(&s3.DeleteObjectInput{
+		Bucket: aws.String(s3Bucket),
+		Key:    aws.String(fileName),
+	})
+	if err != nil {
+		fmt.Printf("Error deleting object: %v\n", err)
+		return
+	}
+	fmt.Printf("File %s deleted successfully\n", fileName)
+}
+
+func moveFile() {
+	target := "processed/meuarquivo.txt"
+	source := "/pending/file0.txt"
+
+	_, err := s3Client.CopyObject(&s3.CopyObjectInput{
+		Bucket:     aws.String(s3Bucket),
+		Key:        aws.String(target),
+		CopySource: aws.String(s3Bucket + source),
+	})
+	if err != nil {
+		fmt.Printf("Error copying object: %v\n", err)
+		return
+	}
+
+	_, err = s3Client.DeleteObject(&s3.DeleteObjectInput{
+		Bucket: aws.String(s3Bucket),
+		Key:    aws.String(source),
+	})
+	if err != nil {
+		fmt.Printf("Error deleting original object: %v\n", err)
+		return
+	}
+	fmt.Printf("File %s copied to %s successfully\n", source, target)
 }
