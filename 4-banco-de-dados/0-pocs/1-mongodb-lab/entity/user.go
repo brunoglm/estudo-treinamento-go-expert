f8b99617437477ac5595ae41a94479e82bf7e26e
Bruno Gabriel <bruno.glm_@hotmail.com>
2025-11-14 23:15:51 -0300

    add content

---
+++ 4-banco-de-dados/0-pocs/1-mongodb-lab/entity/user.go
@@ -0,0 +1,14 @@
+package entity
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty"`
+	Name      string             `bson:"name"`
+	Email     string             `bson:"email"`
+	CreatedAt time.Time          `bson:"created_at"`
+}
---
+++ 4-banco-de-dados/0-pocs/1-mongodb-lab/repository/user/user.go
@@ -0,0 +1,105 @@
+package user
+
+import (
+	"context"
+	"fmt"
+	"mongodb-lab/entity"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type UserStore struct {
+	coll *mongo.Collection
+}
+
+func NewStore(collection *mongo.Collection) *UserStore {
+	return &UserStore{
+		coll: collection,
+	}
+}
+
+func (s *UserStore) CreateUser(ctx context.Context, user *entity.User) (primitive.ObjectID, error) {
+	user.CreatedAt = time.Now()
+
+	opCtx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	result, err := s.coll.InsertOne(opCtx, user)
+	if err != nil {
+		return primitive.NilObjectID, fmt.Errorf("Erro ao inserir usuário: %v", err)
+	}
+
+	id, _ := result.InsertedID.(primitive.ObjectID)
+	return id, nil
+}
+
+func (s *UserStore) GetUserByID(ctx context.Context, id primitive.ObjectID) (*entity.User, error) {
+	opCtx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	var user entity.User
+	filter := bson.M{"_id": id}
+
+	err := s.coll.FindOne(opCtx, filter).Decode(&user)
+	if err == mongo.ErrNoDocuments {
+		return nil, fmt.Errorf("Usuário não encontrado")
+	} else if err != nil {
+		return nil, fmt.Errorf("Erro ao buscar usuário por ID: %v", err)
+	}
+
+	return &user, nil
+}
+
+func (s *UserStore) UpdateUserName(ctx context.Context, id primitive.ObjectID, newName string) (int64, error) {
+	opCtx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	filter := bson.M{"_id": id}
+
+	// pode ser adicionado outros campos para update
+	updateData := bson.M{
+		"$set": bson.M{"name": newName},
+	}
+
+	result, err := s.coll.UpdateOne(opCtx, filter, updateData)
+	if err != nil {
+		return 0, fmt.Errorf("Erro ao atualizar usuário: %v", err)
+	}
+
+	return result.ModifiedCount, nil
+}
+
+func (s *UserStore) ReplaceUser(ctx context.Context, id primitive.ObjectID, updateUser *entity.User) (int64, error) {
+	opCtx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	filter := bson.M{"_id": id}
+
+	updateUser.ID = id
+
+	result, err := s.coll.ReplaceOne(opCtx, filter, updateUser)
+	if err != mongo.ErrNoDocuments {
+		return 0, fmt.Errorf("Usuário não encontrado para substituição")
+	} else if err != nil {
+		return 0, fmt.Errorf("Erro ao substituir usuário: %v", err)
+	}
+
+	return result.ModifiedCount, nil
+}
+
+func (s *UserStore) DeleteUser(ctx context.Context, id primitive.ObjectID) (int64, error) {
+	opCtx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	filter := bson.M{"_id": id}
+
+	result, err := s.coll.DeleteOne(opCtx, filter)
+	if err != nil {
+		return 0, fmt.Errorf("Erro ao deletar usuário: %v", err)
+	}
+
+	return result.DeletedCount, nil
+}
