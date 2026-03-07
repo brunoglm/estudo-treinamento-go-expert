fea065bb3d366294f912d394f3540ee364ebdd43
Bruno Gabriel <bruno.glm_@hotmail.com>
2026-03-07 14:46:00 -0300

    add content

--- projetos-praticos/concorrencia-go-leilao/cmd/auction/main.go
+++ projetos-praticos/concorrencia-go-leilao/cmd/auction/main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"auction-go/configuration/database/mongodb"
+	"context"
 	"log"
 
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	ctx := context.Background()
+
 	if err := godotenv.Load(); err != nil {
 		log.Fatal("Error loading .env file")
+	}
+
+	databaseClient, err := mongodb.NewMongoDBConnection(ctx)
+	if err != nil {
+		log.Fatal("Error connecting to MongoDB:", err)
 	}
 }
